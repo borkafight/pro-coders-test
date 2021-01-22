f6cc0f04d3c5d7813daedcd85653ad1e44770f6b
Pavel Sokolov <frontbastard@gmail.com>
2021-01-22 10:09:31 +0200

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import {Button, Card, Icon} from 'element-ui'
+import {Button, Card, Icon, Input} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 Vue.use(Button)
 Vue.use(Card)
 Vue.use(Icon)
+Vue.use(Input)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
