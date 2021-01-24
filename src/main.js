3def4447eb557721f9b603a99ffb1d5623af9e4a
Pavel Sokolov <frontbastard@gmail.com>
2021-01-25 00:31:56 +0200

    Rework including ElementUI

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import {Button, Card, Icon, Input} from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 
-Vue.use(Button)
-Vue.use(Card)
-Vue.use(Icon)
-Vue.use(Input)
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, {locale})
 
 Vue.config.productionTip = false
 
