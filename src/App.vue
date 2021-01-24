6f4034fe32b3c83efa5e2a56f769bb24f0f64478
Pavel Sokolov <frontbastard@gmail.com>
2021-01-25 00:29:10 +0200

    Remove comments

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <h1 class="title">Projects manager</h1>
-    <!-- <div id="nav">
-      <router-link to="/">Projects</router-link> |
-      <router-link to="/project">Project</router-link>
-    </div> -->
     <router-view />
   </div>
 </template>
@@ -16,7 +11,7 @@
   background-size: cover;
   padding: 0 15px;
   text-align: center;
-  color: #2c3e50;
+  color: $base-text-color;
   overflow: auto;
 }
 
